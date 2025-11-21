dba5aa3d3a05d1e364e5579a941c0d3fd51054b9
Shashikiran1995 <shashijamesbond@gmail.com>
2025-11-21 15:38:39 +0530

    messagehere

--- src/main/java/Mdummy/CMdummy.java
+++ src/main/java/Mdummy/CMdummy.java
@@ -2,6 +2,7 @@ package Mdummy;
 
 public class CMdummy {
 public static void main(String[] args) {
-	System.out.println("this is a main dummy class");
+	System.out.println("changing this main dummy class");
+	System.out.println("another");
 }
 }
---
+++ src/main/java/Mdummy/secClass.java
@@ -0,0 +1,7 @@
+package Mdummy;
+
+public class secClass {
+public static void main(String[] args) {
+	System.out.println("yso");
+}
+}
