4262e5c90a368099f85ce66acc084687b14b70e9
Shashikiran1995 <shashijamesbond@gmail.com>
2025-11-21 15:10:23 +0530

    anymessagehere

---
+++ src/main/java/Mdummy/CMdummy.java
@@ -0,0 +1,7 @@
+package Mdummy;
+
+public class CMdummy {
+public static void main(String[] args) {
+	System.out.println("this is a main dummy class");
+}
+}
---
+++ src/test/java/MTdummy/CTdummy.java
@@ -0,0 +1,7 @@
+package MTdummy;
+
+public class CTdummy {
+	public static void main(String[] args) {
+		System.out.println("this is a test dummy class");
+	}
+}
